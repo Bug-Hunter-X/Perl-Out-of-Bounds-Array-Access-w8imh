10b7ac91b9af0522a187af256288cb2858574d3e
BugHunter-X <askzenai@gmail.com>
2024-12-30 11:00:04 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3);my $size = @array; # Get the size of the arrayif ($size > 0) {my $element = $array[0]; # Access the first elementprint "First element: $element\n";} else {print "Array is empty\n";}
